9c4c0eb3a020b34ae02a0db129da186a75d5578c
varun-program <varunyt.sai@outlook.com>
2026-02-01 23:12:04 +0530

    day 1 completed

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Scanner for user input
+        student stu = new student();// Create a student object for student class
+        System.out.print("Enter student name: ");
+        stu.name = input.nextLine();
+        System.out.print("Enter student roll number: ");
+        stu.rollno = input.nextInt();
+        System.out.print("Enter student age: ");
+        stu.age=input.nextInt();
+
+        IO.print("\n--- Student Details ---\n");
+        stu.displayDetails();
+        }
+        }
---
+++ student.java
@@ -0,0 +1,10 @@
+public class student{
+    String name;
+    int rollno;
+    int age;
+    void displayDetails(){
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollno);
+        System.out.println("age of student is:"+age);
+    }
+}
