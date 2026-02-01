554ccd45b741c721b24a5609a896036d7c869117
varun-program <varunyt.sai@outlook.com>
2026-02-01 23:59:01 +0530

    day 2 compeleted

--- main.java
+++ main.java
@@ -9,6 +9,12 @@ public class main {
         stu.rollno = input.nextInt();
         System.out.print("Enter student age: ");
         stu.age=input.nextInt();
+        System.out.println("Enter marks for  subjects:");
+        for(int i=0;i<stu.subjectmarks.length;i++){
+            stu.subjectmarks[i]=input.nextInt();
+        }
+         stu.sum(); // calling sum method
+         stu.average(); // calling average method
 
         IO.print("\n--- Student Details ---\n");
         stu.displayDetails();
--- student.java
+++ student.java
@@ -2,9 +2,28 @@ public class student{
     String name;
     int rollno;
     int age;
+
+    int[] subjectmarks= new int[5];
+    int totalmarks;
+    double avg;
+
+    void sum(){
+        totalmarks =0;
+        for(int i=0;i<subjectmarks.length;i++){
+            totalmarks+=subjectmarks[i]; //0+arr[0]element 10 to totalmarks=10 
+
+    }}
+    
+
+    void average(){
+        avg=totalmarks/subjectmarks.length;}
+
+
     void displayDetails(){
         System.out.println("Name: " + name);
         System.out.println("Roll No: " + rollno);
-        System.out.println("age of student is:"+age);
+        System.out.println("Age:"+age);
+        System.out.println("Total Marks: " + totalmarks);
+        System.out.println("Average Marks: " + avg);
     }
 }
